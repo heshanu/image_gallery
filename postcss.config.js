9ff7ac172935f91d82a2fe8511a7ee95c5603537
heshan <heshanu97@gmail.com>
2020-10-29 23:09:38 +0530

    add dev

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+const tailwindcss=require('tailwindcss');
+
+
+module.exports ={
+    plugins: [
+        tailwindcss('./tailwind.js'),
+        require('autoprefixer')
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
